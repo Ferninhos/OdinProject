8781265c6b169c6d377aa48348bbe691d94e0c2c
Felipe Castro <jokersxplayer@gmail.com>
2025-02-26 09:57:14 -0300

    Anotações JS

---
+++ Anotações/AnotaçõesJS.js
@@ -0,0 +1,12 @@
+//addEventListener = para ações do usuario ou da propria página(carregamento, dados)
+//querySelector = selecionar algo do HTML
+//querySelectorAll - Transforma em uma nodelist, se quiser transformar para array -> Array.from(nome) ou [...nome]
+//textContext = pega o valor texto de um elemento HTML
+              //ou dá um valor para uma var em um HTML
+//função:
+    //function (event) {} pode ser (event) =>
+    // => indica função significa return
+    // () vazio indica função sem argumento
+//loop:
+    //outer: loops nestados quando quero sair de todos os loops de uma vez
+    //forEach: loop em arrays etc
